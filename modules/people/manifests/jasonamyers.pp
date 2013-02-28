75c383c8bffa130c331a3fa7ebc91fc73c9cc4fb
Jason Myers <jason@jasonamyers.com>
2013-02-28 09:51:31 -0600

    Removing virtualenvwrapper command setup
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- modules/people/manifests/jasonamyers.pp
+++ modules/people/manifests/jasonamyers.pp
@@ -85,16 +85,4 @@ class people::jasonamyers {
   exec { 'dotfilessubmodules':
     command => 'cd /Users/jasonamyers/my/dotfiles && git submodule init && git submodule update'
   }
-
-  exec { 'initialvirtualenvwrapper':
-    command => 'export WORKON_HOME=/Users/jasonamyers/.virtualenv && export VIRTUALENVWRAPPER_PYTHON=/opt/boxen/homebrew/bin/python && export PIP_VIRTUALENV_BASE=$WORKON_HOME && export PIP_RESPECT_VIRTUALENV=true && source /opt/boxen/homebrew/share/python/virtualenvwrapper.sh'
-  }
-
- exec { 'addpostmkvirtualcmd':
-    command => 'echo -e "proj_name=$(echo $VIRTUAL_ENV|awk -F\'/\' \'{print $NF}\')\nmkdir $HOME/projects/$proj_name\nadd2virtualenv $HOME/projects/$proj_name\ncd $HOME/projects/$proj_name" > /Users/jasonamyers/.virtualenv/postmkvirtualenv'
-  }
-
- exec { 'addworkoncmd':
-    command => 'echo -e "proj_name=$(echo $VIRTUAL_ENV|awk -F\'/\' \'{print $NF}\')\ncd $HOME/projects/$proj_name" > /Users/jasonamyers/.virtualenv/postactivate'
-  }
 }
